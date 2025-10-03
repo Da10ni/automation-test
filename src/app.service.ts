be7c3dd3621b6e9205ebdd0f0678581c276a748d
moiz <moiz@brainyboxtech.com>
2025-10-03 20:56:06 +0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import os from "os"
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!!!!';
+    return 'Hello World!!!! checking lint if its workin or not';
   }
 }
